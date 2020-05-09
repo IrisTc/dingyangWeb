cf61a1a5d4832397ca154152d5f7d91e9bd3eb53
IrisTc <1010939908@qq.com>
2020-05-09 14:06:29 +0800

    把dingyang1.0删掉了，有点太大了

--- dingyang1.0/docs/core/vue-router/index.js
+++
@@ -1,3 +0,0 @@
-import VueRouter from './vue-router.js';
-export default VueRouter;
-//# sourceMappingURL=index.js.map
--- dingyang1.0/docs/dingyang/date.js
+++
@@ -1,4 +0,0 @@
-export function convertDate(obj) {
-    return { ...obj, date: new Date(obj.date) };
-}
-//# sourceMappingURL=date.js.map
